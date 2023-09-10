f97032fce425586b177818b0c9c4cf7940b7add6
alexsanderlinn <alexsander.linn@icloud.com>
2023-09-10 00:40:25 -0400

    aula 11

---
+++ exercise-12/script.js
@@ -0,0 +1,10 @@
+var hora = 10
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12){
+    console.log('Bom dia!')
+} else if (18 > hora) {
+        console.log('Boa Tarde!')
+    } else {
+        console.log('Boa noite!')
+    
+ }
