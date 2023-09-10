f07aa01a8f8f4ecb962f6b2f765616f29e37ed02
alexsanderlinn <alexsander.linn@icloud.com>
2023-09-10 11:33:48 -0400

    aula 14

--- exercise-14/js/script.js
+++ exercise-14/js/script.js
@@ -1,7 +1,34 @@
     function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${horas} horas.`
+        
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var min = new Date();
+    var sec = new Date();
+    var hour = data.getHours();
+    var minute = min.getMinutes();
+    var seconds = sec.getSeconds();
+
+
+    msg.innerHTML = `Agora são ${hour} horas e ${minute} Minutos e ${seconds} Segudos`;
+
+
+    if (hour >= 0 && hour < 12) {
+        //BOM DIA!
+        img.src = '../img/morning.png';
+        document.body.style.background = '#5fa0ea9a';
+    } else if (hour >= 12 && hour <= 18) {
+        //BOA TARDE!
+        img.src = '../img/afternoon.png';
+        document.body.style.background = '#0522059d';
+    } else {
+        //BOA NOITE!
+        img.src = '../img/night.png';
+        document.body.style.background = '#0c2848cc';
+    }
+
+
+    setTimeout(function(){ location.reload(); }, 2000);
+
+
     }
