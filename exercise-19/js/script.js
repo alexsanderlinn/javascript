a6774a2c59916c2cc902fb05fbe88ead68625ce0
alexsanderlinn <alexsander.linn@icloud.com>
2023-11-14 16:25:30 -0400

    update

---
+++ exercise-19/js/script.js
@@ -0,0 +1,7 @@
+var s = document.getElementById('start')
+var e = document.getElementById('end')
+var st = document.getElementById('step')
+
+if s.value.length == 0 || Number(fano.value {
+    window.alert
+}
