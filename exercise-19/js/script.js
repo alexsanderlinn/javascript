1fa4956d8f54f763c267caf62830bd83be366bd7
alexsander <alexsanderlinn2@gmail.com>
2023-12-24 11:52:19 -0400

    exercise-19

--- exercise-19/js/script.js
+++ exercise-19/js/script.js
@@ -1,7 +1,28 @@
-var s = document.getElementById('start')
-var e = document.getElementById('end')
-var st = document.getElementById('step')
+function contagem() {
+    var start = document.getElementById('startInput').value;
+    var end = document.getElementById('endInput').value;
+    var step = document.getElementById('stepInput').value;
 
-if s.value.length == 0 || Number(fano.value {
-    window.alert
+    var res = document.getElementById('result');
+    res.innerHTML = "";  // Limpa o conteúdo anterior
+
+    // Verifica se os valores são válidos
+    if (isNaN(start) || isNaN(end) || isNaN(step)) {
+        res.innerHTML = "Por favor, insira números válidos.";
+        return;
+    }
+
+    // Inicializa uma variável para armazenar o valor atual
+    var current = start;
+
+    //Realiza a contagem
+    while (current <= end) {
+        res.innerHTML += `Passo ${current}<br>`;
+        current += step;
+    }
+}
+
+function refresh(){
+    //Atualiza a Pagina
+    location.reload()
 }
