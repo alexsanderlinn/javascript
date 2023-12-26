f3e895fdcda57e17dd249788bde4c918a01c6ea0
alexsander <alexsanderlinn2@gmail.com>
2023-12-26 18:19:38 -0400

    update.
    colocando uma trava para caso o campo estiver vazio.

--- exercise-20/js/script.js
+++ exercise-20/js/script.js
@@ -1,11 +1,14 @@
 function search() {
     var num = parseFloat(document.getElementById('number-input').value);
   
-    for (let i = 0; i <= 10; i++) {
-      document.querySelector(`.res${i}`).innerHTML = `${i} x ${num} = ${i * num}`;
+    if (isNaN(num) || num === "") {
+        window.alert('[ERRO 01] Number empty, please enter with number')
+    } else {
+        for (let i = 0; i <= 10; i++) {
+            document.querySelector(`.res${i}`).innerHTML = `${i} x ${num} = ${i * num}`;
+          }
     }
-  }
-  
+}
 function refresh() {
     location.reload();
 }
